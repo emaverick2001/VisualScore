23b97abd10f08add08daf4567eca2e62f5852aac
Mav <mespin11@jh.edu>
2024-03-20 19:49:14 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from PySide6.QtWidgets import QApplication, QMainWindow,QPushButton
+
+import sys
+
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button Holder app")
+        button = QPushButton("Click Me!")
+        self.setCentralWidget(button)  
+
+app = QApplication(sys.argv)
+
+window = ButtonHolder()
+window.show()
+app.exec()
